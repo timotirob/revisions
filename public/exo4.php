2d6ab6abc4b40b78f0ef27db7e9dfd5bd44d3018
Timothée Robert <timothee.robert@enc-bessieres.org>
2026-01-14 13:04:41 +0100

    Avec Exo4 corrigé par Coco

--- public/exo4.php
+++ public/exo4.php
@@ -7,8 +7,17 @@ require_once 'exoPlanning.php';
 
 // À partir d'ici, les variables $plannings, $salles, $enseignants existent !
 
-echo "<h1>Exercices Planning</h1>";
+echo "<h1>Exercices Planning</h1> \r\n";
 
 // Test Exercice 4 (Exemple)
 $totalHeures = 0;
-// A compléter
+//Parcourir les données du tableau plannings
+foreach ($plannings as $planning) {
+    //filtre pour spécifier la semaine visée ici S42
+    if ($planning->getSemaine() === 'S42'){
+        //cumul des heures
+        $totalHeures += $planning->getDuree();
+    }
+}
+//affichage
+echo "Total heures pour la semaine S42 ".$totalHeures." heures";
